459210971f5724a870fd1ed9ee0ba5a41668ee19
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2019-04-29 09:52:51 +0700

    logout function done

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,12 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   })
 
 );
-console.log(req.user);
+// console.log(req.user);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/login');
+});
 
 module.exports = router;
